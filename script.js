b1512190ad7c1a593fa581aa577363c2d0c9795b
Jasmine Erickson <sacredlightstudios@gmail.com>
2023-04-24 17:05:09 -0500

    Adding random response algo

--- script.js
+++ script.js
@@ -68,3 +68,32 @@ function d6() {
 var playerRoll = d6();
 console.log("The player rolled: " + playerRoll);
 
+//write a function that returns random responses
+var lifesAnswers = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes – definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful."
+];
+function randomAnswer(){
+    var randomIndex = Math.floor(Math.random() * lifesAnswers.length);
+return lifesAnswers[randomIndex];
+}
+var answer = randomAnswer();
+console.log(answer);
